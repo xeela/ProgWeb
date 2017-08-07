cd05df9095367704f6927bc63ae950dcd10180d7
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2017-08-07 12:35:49 +0200

    create_table

---
+++ insert_users.sql
@@ -0,0 +1,42 @@
+﻿describe shops;
+describe coordinates;
+describe shop_coordinate;
+describe users;
+
+#5 citta, ognuna con 10 negozi. 
+#1) creo gli utenti (min 10): ogni negozio ha un propietario e un "creatore" non per forza uguli
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Francesco", "Bruschetti", "fbrus", "f.b@yahoo.it","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Davide", "Farina", "pilotapazzo", "davide.farina@yahoo.it","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Gianluca", "Pasqua", "gianp", "gianluca.pasqua@yahoo.it","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Caterina", "Battisti", "cate", "caterian.battisti@gmail.com","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Michela", "Comperini", "michi", "michela.c@yahoo.it","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Steve", "Jobs", "sjobs", "steve.jobs@icloud.com","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("Christian", "Dorighelli", "chdorig", "info@dorighelli.it","pass");
+insert into users (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS) values ("John", "Muller", "jm_husq", "johnmuller@hotmail.it","pass");
+#.... gli altri verranno aggiunti dal sito web
+
+#per ogni città almeno 20 recensioni 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
